1ffea881c88b25c95ed170c68c620e33d470c12b
Jan Skála <j.skala@hotmail.com>
2020-03-11 19:27:59 +0100

    Add KO.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,42 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="app">
+      <div v-for="subject in subjects" :key="subject.code">
+        <h2>{{subject.name}}</h2>
+        <h3>{{subject.code}}</h3>
+        <div class="link-group">
+          <p v-for="link in subject.links" :key="link">
+            <img src="../assets/link.png" />
+            <a target="_blank" :href="link.address">{{link.name}}</a>
+          </p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  data() {
+    return {
+      msg: "Hello",
+      subjects: [
+        {
+          name: "Combinatorial Optimization",
+          code: "B4M35KO",
+          links: [
+            {
+              name: "Course Ware",
+              address: "https://cw.fel.cvut.cz/wiki/courses/ko/start"
+            }
+          ]
+        }
+      ]
+    };
   }
-}
+});
 </script>
+
+<style lang="less">
+</style>
