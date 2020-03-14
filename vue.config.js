b5d6965557afdc490866e67dfa84fb00982115c2
Jan Skála <j.skala@hotmail.com>
2020-03-14 15:27:41 +0100

    Publish to dist.

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 
 module.exports = {
-    outputDir: path.resolve(__dirname, "docs"),
+    outputDir: path.resolve(__dirname, "dist"),
     publicPath: process.env.NODE_ENV === 'production'
         ? '/'
         : '/'
