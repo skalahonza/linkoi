b50e1f13f8766b44bc1fe27f0c95b4b137f77e2a
Jan Skála <j.skala@hotmail.com>
2020-03-16 13:34:36 +0100

    Add useful links.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,44 +1,73 @@
 <template>
-  <div class="home">    
-      <b-card
-        v-for="subject in subjects"
-        :key="subject.code"
-        :title="subject.name"
-        :sub-title="subject.code"
-      >
-        <b-button
-          pill
-          v-for="link in subject.links"
-          :key="link.address"
-          target="_blank"
-          :href="link.address"                                          
-          v-bind:variant="linkType(link.name)"          
-        >{{link.name}}</b-button>
-      </b-card>    
+  <div class="home">
+    <b-card
+      v-for="subject in subjects"
+      :key="subject.code"
+      :title="subject.name"
+      :sub-title="subject.code"
+    >
+      <b-button
+        pill
+        v-for="link in subject.links"
+        :key="link.address"
+        target="_blank"
+        :href="link.address"
+        v-bind:variant="linkType(link.name)"
+      >{{link.name}}</b-button>
+    </b-card>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-export default Vue.extend({ 
-  methods:{
-    linkType: function (name: string): string{      
-      switch(name){
-        case "Moodle": return "primary";
-        case "Courseware": return "success";
-        case "Teams": return "info";
-        default: return "secondary";
+export default Vue.extend({
+  methods: {
+    linkType: function(name: string): string {
+      switch (name) {
+        case "Moodle":
+          return "primary";
+        case "Courseware":
+          return "success";
+        case "Teams":
+          return "info";
+        case "Office 365":
+          return "danger";
+        case "Brute":
+          return "dark";
+        default:
+          return "secondary";
       }
     }
-  }, 
+  },
   data() {
     return {
       subjects: [
         {
+          name: "Useful Links",
+          code: "",
+          links: [
+            {
+              name: "Office 365",
+              address: "https://office365.cvut.cz"
+            },
+            {
+              name: "Lectures/Labs recordings",
+              address: "https://web.microsoftstream.com/"
+            },
+            {
+              name: "Moodle",
+              address: "https://moodle.fel.cvut.cz/"
+            },
+            {
+              name: "Brute",
+              address: "https://cw.felk.cvut.cz/brute/student/"
+            }
+          ]
+        },
+        {
           name: "Combinatorial Optimization",
           code: "B4M35KO",
           links: [
-
             {
               name: "Courseware",
               address: "https://cw.fel.cvut.cz/wiki/courses/ko/start"
@@ -51,7 +80,8 @@ export default Vue.extend({
           links: [
             {
               name: "Math FELD",
-              address: "http://math.feld.cvut.cz/demlova/teaching/tal_vyuka.html"
+              address:
+                "http://math.feld.cvut.cz/demlova/teaching/tal_vyuka.html"
             },
             {
               name: "Moodle",
@@ -69,7 +99,8 @@ export default Vue.extend({
             },
             {
               name: "Teams",
-              address: "https://teams.microsoft.com/l/team/19%3a465c54787d7e4b9b8740e58eeb9a5a55%40thread.tacv2/conversations?groupId=02e6b8d0-78a5-4e49-8367-9b952bc4cfac&tenantId=f345c406-5268-43b0-b19f-5862fa6833f8"
+              address:
+                "https://teams.microsoft.com/l/team/19%3a465c54787d7e4b9b8740e58eeb9a5a55%40thread.tacv2/conversations?groupId=02e6b8d0-78a5-4e49-8367-9b952bc4cfac&tenantId=f345c406-5268-43b0-b19f-5862fa6833f8"
             }
           ]
         },
@@ -83,7 +114,8 @@ export default Vue.extend({
             },
             {
               name: "Teams",
-              address: "https://teams.microsoft.com/l/team/19%3a5aad055a96f94e8abba8ad6fdffc0584%40thread.tacv2/conversations?groupId=5df990ab-0cb2-4268-acf9-53d5d14b4a78&tenantId=f345c406-5268-43b0-b19f-5862fa6833f8"
+              address:
+                "https://teams.microsoft.com/l/team/19%3a5aad055a96f94e8abba8ad6fdffc0584%40thread.tacv2/conversations?groupId=5df990ab-0cb2-4268-acf9-53d5d14b4a78&tenantId=f345c406-5268-43b0-b19f-5862fa6833f8"
             }
           ]
         },
